2dbdb15fee0af4f4621b8b502ae24f69db2285b1
Kana <andrew4chrome@gmail.com>
2023-01-28 10:51:24 +0400

    fix runtime.c tag matching

--- runtime/runtime.c
+++ runtime/runtime.c
@@ -3,17 +3,16 @@
 
 # include "../src/Language/Agner/X64.h"
 
-extern void _print_value(int64_t n) {
-  int64_t tag = n & TAG_SIZE;
-  switch (tag) {
+extern void _print_value(int64_t value) {
+  switch (value & TAG_MASK) {
     case NUMBER_TAG:
-      printf("%lld\n", n >> 3);
+      printf("%lld\n", value >> 3);
       break;
     case UNBOUND_TAG:
       printf("unbound var");
       break;
     case ATOM_TAG:
-      printf("%s\n", (char *) n);
+      printf("%s\n", (char *) value);
       break;
   }
   fflush(stdout);
