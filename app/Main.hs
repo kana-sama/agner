9d982bc196e34b2bebe3ad03a54615525ecd6759
Kana <andrew4chrome@gmail.com>
2023-03-09 21:16:49 +0400

    add compile command

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@ import Data.Aeson (encodeFile)
 import System.Process.Typed (runProcess, shell, proc)
 import System.IO.Temp (withSystemTempDirectory)
 import System.Exit (ExitCode(..), exitFailure)
-import System.FilePath ((</>), (<.>))
+import System.FilePath ((</>), (<.>), stripExtension)
 import System.Environment (getArgs)
 import System.Info (os)
 
@@ -23,6 +23,10 @@ import Language.Agner.Prettier qualified as Prettier
 import Language.Agner.Linter qualified as Linter
 import Language.Agner.Optimizer (optimize)
 
+data Command
+  = Compile{target :: Maybe X64.Target, source :: FilePath, output :: Maybe FilePath}
+  | Example{target :: Maybe X64.Target}
+
 run :: forall e. Exception e => IO Value -> IO ()
 run value = do
   result <- try @e (evaluate =<< value)
@@ -30,20 +34,82 @@ run value = do
     Left e -> putStrLn (displayException e)
     Right v -> putStrLn (Value.encode v)
 
-parseArgs :: IO X64.Target
+parseArgs :: IO Command
 parseArgs = do
-  args <- getArgs
-  pure $! case args of
-    ["--target", "linux"] -> X64.Linux
-    ["--target", "macos"] -> X64.MacOS
-    [] | "linux"  <-os-> X64.Linux
-       | "darwin" <-os-> X64.MacOS
+  getArgs >>= \case
+    [] -> pure Example{target = Nothing}
+    ["--target", "linux"] -> pure Example{target = Just X64.Linux}
+    ["--target", "macos"] -> pure Example{target = Just X64.MacOS}
+    [source] -> pure Compile{target = Nothing, source, output = Nothing}
+    [source, "-o", output] -> pure Compile{target = Nothing, source, output = Just output}
+    [source, "-o", output] -> pure Compile{target = Nothing, source, output = Just output}
+    _ -> error "неправильные аргументы"
+
+getTarget :: Maybe X64.Target -> X64.Target
+getTarget (Just target) = target
+getTarget Nothing =
+  case os of
+    "linux" -> X64.Linux
+    "darwin" -> X64.MacOS
     _ -> error "Иди отсюда, пёс"
 
-main :: IO ()
-main = do
-  target <- parseArgs
+getOutput :: FilePath -> Maybe FilePath -> FilePath
+getOutput source (Just path) = path
+getOutput source Nothing =
+  case stripExtension "agn" source of
+    Nothing -> error "unknown extension"
+    Just path -> path
 
+compile ::  
+  "target" :! X64.Target ->
+  "source" :! FilePath ->
+  "output" :! FilePath ->
+  IO ()
+compile (Arg target) (Arg source) (Arg output) = do
+  -- parse
+  !source <- try @Parser.Ex (evaluate =<< Parser.parse Parser.module_ <$> readFile source) >>= \case
+    Right source -> pure source
+    Left ex -> do putStrLn (displayException ex); exitFailure
+
+  -- lint
+  case Linter.check source of
+    Nothing -> pure ()
+    Just error -> do
+      putStrLn ("** linter error: " ++ Linter.prettyError error)
+      exitFailure
+
+  -- optimize
+  source <- pure (optimize source)
+
+  let sm = SM.compileModule source
+
+  -- compile
+  withSystemTempDirectory "test" \path -> do
+    let gas = (X64.prettyProg . X64.compile target) sm
+
+    let runtimePath = "." </> "runtime" </> "runtime.c"
+    let sourcePath = path </> "temp" <.> "s"
+    let outputPath = output
+
+    writeFile sourcePath gas
+    gcc ! param #target target
+        ! param #source sourcePath
+        ! param #runtime runtimePath
+        ! param #output outputPath
+
+  pure ()
+
+main :: IO ()
+main = parseArgs >>= \case
+  Compile{target, source, output} ->
+    compile ! param #target (getTarget target)
+            ! param #source source
+            ! param #output (getOutput source output)
+  Example{target} ->
+    example (getTarget target)
+
+example :: X64.Target -> IO ()
+example target = do
   -- parse
   !source <- try @Parser.Ex (evaluate =<< Parser.parse Parser.module_ <$> readFile "example.agn") >>= \case
     Right source -> pure source
